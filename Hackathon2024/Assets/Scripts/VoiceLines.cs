7a542d8b97e15ffdd82b2b508a3b066d435fefd3
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2024-03-23 19:42:44 -0700

    SOUNDS
    SOUNDS SOUNDS

---
+++ Hackathon2024/Assets/Scripts/VoiceLines.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceLines : MonoBehaviour
+{
+
+    public List<AudioSource> voiceLines, randVoiceLines;
+    private int randNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RandomLines());
+    }
+
+    private IEnumerator RandomLines()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(45);
+            randNum = UnityEngine.Random.Range(0, 10);
+            randVoiceLines[randNum].Play();
+        }
+    }
+
+}
